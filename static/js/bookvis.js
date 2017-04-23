e92fc5b5fe64f1c659fdce92897d27fd93d9f83f
Sam Szreter <s.szreter@gmail.com>
2017-04-23 19:49:40 +0100

    Make relative lengths work between book series
    Made relative lengths work between different series by updating the
    function to a map over the series.
    Changed data to be in js files instead of csv as it seems neater and
    csv didn't seem to work with the map.
    Also changed some styling so books look slighlty leaning.

--- static/js/bookvis.js
+++ static/js/bookvis.js
@@ -1,76 +1,43 @@
 //creates svg elements for each book in collection with correct height and
 //width
-function drawShelf(selector, csvData) {
-	var jsonData = jsonifyThreeTier(csvData).books;
-	var container = d3.select(selector);
+selectors = ['#worm', '#hp']
 
-	var maxWords = Math.max(...jsonData.map((o) => o.wordCount))
+series = [worm_data, hp_data]
+
+function getLongestBook(books) {
+	return Math.max(...books.map((b) => b.wordCount))
+}
+
+function drawShelves(series) {
+	longestBookLengths = series.map((series) => getLongestBook(series));
+	longestBookLength = Math.max(...longestBookLengths);
+	series.map((s, i) => {
+		let selector = selectors[i];
+		drawShelf(selector, s, longestBookLength)
+	})
+}
+
+drawShelves(series);
+
+function drawShelf(selector, books, longestBookLength) {
+	var height = window.innerHeight * 0.7;
+	var width = height * 0.3;
+	var container = d3.select(selector);
 	var widthScale = d3.scale.linear()
-						.domain([0, maxWords])
-						.range([0, 150]);
+						.domain([0, longestBookLength])
+						.range([0, width]);
 
 	var books = container.selectAll('.book')
-				   .data(jsonData);
+				   .data(books);
 
 	var booksEnter = books.enter()
 				   .append('div')
 				   .attr('class', 'book')
-				   .style('height', () => window.innerHeight * 0.8 + 'px')
+				   .style('height', () => height + 'px')
 				   .style('min-width', (d) => widthScale(d.wordCount))
 				   .style('max-width', (d) => widthScale(d.wordCount));
 
 	booksEnter.append('div')
 					.attr('class', 'title')
-					.style('transform', (d, i) => {
-						let books = document.querySelectorAll('.book');
-						let thisBook = books[i];
-						let bookWidth = thisBook.clientWidth;
-						return 'translate(' + (bookWidth / 2 - 8) + 'px, 0px) rotate(90deg)'
-				})
 					.text((d) => d.title);
-	};
-
-	// books.on('mouseover', function(d) {
-	// 	var multiplier = 1;
-	// 	var chapterBreakdown = d3.select('#chapterBreakdown');
-	// 	var title = chapterBreakdown.append('div')
-	// 							  .classed('chapters', true)
-	// 							  .html(d['title']);
-	// 	var chapterDiv = chapterBreakdown.append('div')
-	// 							  .classed('chapters', true)
-	// 							  .style('margin', 'auto');
-	// 	var chapters = chapterDiv.selectAll('svg')
-	// 					.data(d['chapters'])
-	// 					.enter()
-	// 					.append('svg')
-	// 				    .attr('overflow', 'visible')
-	// 				    .attr('height', multiplier * heightScale(1))
-	// 		   		    .attr('width', function(d) {
-	// 		   		   					return multiplier * widthScale(d.wordCount);
-	// 		   		   					});
-
-	// 	var numbers = ['1', '2', '3', '4', '5',
-	// 			       '6', '7', '8', '9', '10',
-	// 			       '11', '12', '13', '14'];
-
-	// 	var chapterOutlines = chapters.append('rect')
- //                    .attr('x', 0)
- //                    .attr('y', 0)
-	// 				.attr('height', multiplier * heightScale(1))
-	// 		   		.attr('width', function(d) {
-	// 		   		   					return multiplier * widthScale(d.wordCount);
-	// 		   		   					})
- //                    .style('stroke', 'black')
- //                    .style('stroke-width', 0.5)
- //                    .style('fill', function(d) {
- //                    			try {
-	//                     			if (numbers.indexOf(d.title.split('.')[1]) != -1)
-	//                     			{return 'none';}
-	//                     			else {return 'grey';}
-	//                     		}
-	//                     		catch(TypeError) {return 'none';}
-	//                     		});
-
-
-d3.csv('data/worm.csv', drawShelf.bind(null, '#worm'))
-d3.csv('data/hp.csv', drawShelf.bind(null, '#hp'))
+};
---
+++ static/js/hp_data.js
@@ -0,0 +1,30 @@
+hp_data = [
+ {
+   "title": "Philosopher's Stone",
+   "wordCount": 76944
+ },
+ {
+   "title": "Chamber of Secrets",
+   "wordCount": 85141
+ },
+ {
+   "title": "Prisoner of Azkaban",
+   "wordCount": 107253
+ },
+ {
+   "title": "Goblet of Fire",
+   "wordCount": 190637
+ },
+ {
+   "title": "Order of the Phoenix",
+   "wordCount": 257045
+ },
+ {
+   "title": "Half-Blood Prince",
+   "wordCount": 168923
+ },
+ {
+   "title": "Deathly Hallows",
+   "wordCount": 198227
+ }
+]
