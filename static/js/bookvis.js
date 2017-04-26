9e67a94575cc9abb5cbe787b4ebd97e0c4e95641
Sam Szreter <s.szreter@gmail.com>
2017-04-26 23:40:44 +0100

    Remove all arrow functions and 'let's

--- static/js/bookvis.js
+++ static/js/bookvis.js
@@ -1,6 +1,7 @@
 //creates svg elements for each book in collection with correct height and
 //width
 var bookBorderWidth = 1;
+var shelfHeight = '60px';
 
 ids = ['worm', 'hp']
 titles = ['Worm', 'Harry Potter']
@@ -8,21 +9,33 @@ colourOffsets = [90, 220]
 
 series = [worm_data, hp_data]
 
+function pxToInt(px) {
+	return parseInt(px.replace('px', ''));
+}
+
 function getBodyWidth() {
 	var windowWidth = window.innerWidth;
 	var body = document.querySelector('body');
 	var bodyMargin = window.getComputedStyle(body, null).margin;
-	var bodyWidth = window.innerWidth - 2 * parseInt(bodyMargin.replace('px', ''));
+	var bodyWidth = windowWidth - 2 * pxToInt(bodyMargin);
 	return bodyWidth
 }
 
+function getWindowHeight() {
+	var windowHeight = window.innerHeight;
+	var body = document.querySelector('body');
+	var bodyMargin = window.getComputedStyle(body, null).margin;
+	var bodyHeight = windowHeight - pxToInt(shelfHeight) - 2 * pxToInt(bodyMargin);
+	return bodyHeight;
+}
+
 function getLongestBook(books) {
-	return Math.max(...books.map((b) => b.wordCount))
+	return Math.max(...books.map(function(b) {return b.wordCount;}));
 }
 
 function getLongestSeries(series) {
-	var seriesLengths = series.map((o) => {
-		return o.map((p) => p.wordCount ).reduce((a,b) => a + b )
+	var seriesLengths = series.map(function(o) {
+		return o.map(function (p) { return p.wordCount }).reduce(function(a,b) { return a + b });
 	});
 	return Math.max(...seriesLengths)
 }
@@ -36,7 +49,8 @@ function getShelfDiv(id, title) {
 		shelf.append('div')
 			.classed('bookspace', true);
 		var wood = shelf.append('div')
-			.classed('wood', true);
+			.classed('wood', true)
+			.style('height', shelfHeight);
 		wood.append('div')
 			.classed('shelfTitle', true)
 			.text(title);
@@ -46,10 +60,10 @@ function getShelfDiv(id, title) {
 
 function drawShelves(series) {
 	var longestSeries = getLongestSeries(series);
-	series.map((s, i) => {
-		let id = ids[i];
-		let title = titles[i];
-		let colourOffset = colourOffsets[i];
+	series.map(function(s, i) {
+		var id = ids[i];
+		var title = titles[i];
+		var colourOffset = colourOffsets[i];
 		var shelfDiv = getShelfDiv(id, title);
 		drawShelf(shelfDiv, s, longestSeries, colourOffset);
 	})
@@ -58,12 +72,19 @@ function drawShelves(series) {
 drawShelves(series);
 
 function drawShelf(shelfDiv, books, longestSeries, colourOffset) {
-	var bodyWidth = getBodyWidth();
 	var totalBookBorderWidth = bookBorderWidth * books.length * 2;
 
-	var svgWidth = bodyWidth - totalBookBorderWidth;
+	if (window.innerWidth >= 900) {
+		var bodyWidth = getBodyWidth();
+		var svgWidth = bodyWidth - totalBookBorderWidth;
+		var svgHeight = svgWidth * 0.36;
+	} else {
+		var windowHeight = getWindowHeight();
+		var svgHeight = windowHeight - bookBorderWidth * 2;
+		var svgWidth = svgHeight / 0.36;
+	}
 
-	var height = svgWidth * 0.36;
+	console.log(svgWidth);
 
 	var widthScale = d3.scale.linear()
 						.domain([0, longestSeries])
@@ -77,30 +98,26 @@ function drawShelf(shelfDiv, books, longestSeries, colourOffset) {
 
 	// update
 	books
-		.style('height', () => height + 'px')
-		.style('min-width', (d) => widthScale(d.wordCount))
-		.style('max-width', (d) => widthScale(d.wordCount));
+		.style('height', function() { return svgHeight + 'px'; })
+		.style('min-width', function(d) { return widthScale(d.wordCount); })
+		.style('max-width', function(d) { return widthScale(d.wordCount); });
 
 	// enter
 	var booksEnter = books.enter()
 		.append('div')
 			.attr('class', 'book')
-			.style('height', () => height + 'px')
-			.style('min-width', (d) => widthScale(d.wordCount))
-			.style('max-width', (d) => widthScale(d.wordCount))
-			.attr('title', (d) => Math.round(d.wordCount / 1000).toLocaleString() + 'k words')
-			.style('background', (d) => {
+			.style('height', function() { return svgHeight + 'px'; })
+			.style('min-width', function(d) { return widthScale(d.wordCount); })
+			.style('max-width', function(d) { return widthScale(d.wordCount); })
+			.attr('title', function(d) { return Math.round(d.wordCount / 1000).toLocaleString() + 'k words'; })
+			.style('background', function(d) {
 				var mainSaturation = (Math.round(Math.random() * 15) + 20);
 				var hue = Math.round(Math.random() * 60) + colourOffset;
 				return 'linear-gradient(261deg , hsla(' + hue + ',100%,77%,0.8), hsla(' + hue + ',100%,' + mainSaturation + '%,0.8), hsla(' + hue + ',100%,10%,0.92)), url(static/css/grilled.png)';
 		})
 		.append('div')
 			.attr('class', 'title')
-			.text((d) => d.title);
-
-	var wood = shelfDiv.select('.wood')
-		.style('max-width', bodyWidth + 10)
-		.style('min-width', bodyWidth + 10);
+			.text(function(d) { return d.title; });
 
 };
 
