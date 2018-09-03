23a8f2d99afdd6c19645c10f3521834a9f993e88
Sam Szreter <s.szreter@gmail.com>
2018-09-03 23:22:25 +0300

    Realign data

--- src/hp_data.js
+++ src/hp_data.js
@@ -1,32 +1,32 @@
 const hp_data = [
- {
-   "title": "Philosopher's Stone",
-   "wordCount": 76944
- },
- {
-   "title": "Chamber of Secrets",
-   "wordCount": 85141
- },
- {
-   "title": "Prisoner of Azkaban",
-   "wordCount": 107253
- },
- {
-   "title": "Goblet of Fire",
-   "wordCount": 190637
- },
- {
-   "title": "Order of the Phoenix",
-   "wordCount": 257045
- },
- {
-   "title": "Half-Blood Prince",
-   "wordCount": 168923
- },
- {
-   "title": "Deathly Hallows",
-   "wordCount": 198227
- }
-]
+    {
+        "title": "Philosopher's Stone",
+        "wordCount": 76944
+    },
+    {
+        "title": "Chamber of Secrets",
+        "wordCount": 85141
+    },
+    {
+        "title": "Prisoner of Azkaban",
+        "wordCount": 107253
+    },
+    {
+        "title": "Goblet of Fire",
+        "wordCount": 190637
+    },
+    {
+        "title": "Order of the Phoenix",
+        "wordCount": 257045
+    },
+    {
+        "title": "Half-Blood Prince",
+        "wordCount": 168923
+    },
+    {
+        "title": "Deathly Hallows",
+        "wordCount": 198227
+    }
+];
 
 module.exports = hp_data;
