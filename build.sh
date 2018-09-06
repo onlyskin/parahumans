6fe97434e15e313563cad3562934ccef4d2de996
Sam Szreter <s.szreter@gmail.com>
2018-09-06 18:02:33 +0300

    Update build and rename deploy

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ BROWSERIFY=node_modules/browserify/bin/cmd.js
 rm -rf $OUT
 mkdir -p $OUT
 
-$BROWSERIFY $SRC/index.js -o $OUT/index.bundle.js
+$BROWSERIFY $SRC/index.js -o $OUT/index.bundle.js --debug
 
 cp index.html $OUT/index.html
 cp css/* $OUT/
