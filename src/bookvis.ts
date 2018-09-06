8279c2bb08353a0d1e0d0f56842e2f6b5b240242
Sam Szreter <s.szreter@gmail.com>
2018-09-06 18:32:09 +0300

    Add extra type annotations

--- src/bookvis.ts
+++ src/bookvis.ts
@@ -17,25 +17,25 @@ function formatWordCount(book: IBook): string {
     return Math.round(book.wordCount / 1000).toLocaleString() + 'k words';
 }
 
-function randomHueVariant(colourOffset: number) {
+function randomHueVariant(colourOffset: number): string {
     const saturation = (Math.round(Math.random() * 15) + 20);
     const hue = Math.round(Math.random() * 60) + colourOffset;
     return `linear-gradient(351deg, hsla(${hue},100%,77%,0.8), hsla(${hue},100%,${saturation}%,0.8), hsla(${hue},100%,10%,0.92)), url(grilled.png)`;
 }
 
-function randomTranslate() {
+function randomTranslate(): string {
     const maxOffset = 5;
     const offset = Math.random() * maxOffset * 2 - maxOffset;
     return `translateX(${offset}px)`
 }
 
-function updateShelf(series_data: ISeries, root: Element) {
+function updateShelf(series_data: ISeries, root: Element): void {
     const heightScale = d3.scaleLinear()
         .domain([0, 1])
         .range([0, HEIGHT_TO_WIDTH_PIXEL_RATIO_PER_WORD * root.clientWidth]);
 
 	const updating = d3.select(root).selectAll('.book')
-        .data(series_data.books);
+        .data<IBook>(series_data.books);
 
     const entering = updating
         .enter()
